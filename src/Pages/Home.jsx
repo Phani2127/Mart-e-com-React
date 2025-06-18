75784cedeba4f5e3cc83699f61067d0d995275dd
unknown <phaneendraguduru6@gmail.com>
2025-06-18 10:16:25 +0530

    Added best sales part

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -84,6 +84,15 @@ export default function Home() {
           )}
         </div>
       </div>
+      {/* Best Sales */}
+      <div className="my-20">
+        <h1 className="font-bold text-3xl text-center my-10">Best Sales</h1>
+        <div className="flex mx-40   ms-45 flex-wrap gap-10 ">
+          {products.map((item, index) =>
+            item.category === "sofa" ? <Card key={index} item={item} /> : " "
+          )}
+        </div>
+      </div>
     </div>
   );
 }
